38d76a29dd200a856109b3ad91976c9c57009d5d
austin006 <austinmcglashan1@gmail.com>
2026-01-21 17:23:49 -0700

    Bishop, Rook, and Queen moves are working.

--- shared/src/main/java/chess/PieceCalculators/BishopMovesCalculator.java
+++ shared/src/main/java/chess/PieceCalculators/BishopMovesCalculator.java
@@ -2,8 +2,10 @@ package chess.PieceCalculators;
 
 import chess.ChessBoard;
 import chess.ChessMove;
+import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,6 +13,34 @@ public class BishopMovesCalculator implements PieceMovesCalculator{
 
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
-        return List.of();
+        List<ChessMove> moves = new ArrayList<>();
+
+        int[][] directions = {{1,1}, {1,-1}, {-1,1}, {-1, -1}};
+
+        for(int[] direction : directions) {
+            int row = position.getRow();
+            int col = position.getColumn();
+
+            while(true) {
+                row += direction[0];
+                col += direction[1];
+
+                if(row > 8 || row < 1 || col < 1 || col > 8) break;
+
+                ChessPosition potentialSpace = new ChessPosition(row, col);
+                ChessPiece pieceAtSpace = board.getPiece(potentialSpace);
+
+                if(pieceAtSpace == null) {
+                    moves.add(new ChessMove(position, potentialSpace, null));
+                }
+                else {
+                    if(pieceAtSpace.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                        moves.add(new ChessMove(position, potentialSpace, null));
+                    }
+                    break;
+                }
+            }
+        }
+        return moves;
     }
 }
--- shared/src/main/java/chess/PieceCalculators/RookMovesCalculator.java
+++ shared/src/main/java/chess/PieceCalculators/RookMovesCalculator.java
@@ -2,14 +2,44 @@ package chess.PieceCalculators;
 
 import chess.ChessBoard;
 import chess.ChessMove;
+import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class RookMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
-        return List.of();
+        List<ChessMove> moves = new ArrayList<>();
+
+        int[][] directions = {{0,1}, {0,-1}, {-1,0}, {1, 0}};
+
+        for(int[] direction : directions) {
+            int row = position.getRow();
+            int col = position.getColumn();
+
+            while(true) {
+                row += direction[0];
+                col += direction[1];
+
+                if(row > 8 || row < 1 || col < 1 || col > 8) break;
+
+                ChessPosition potentialSpace = new ChessPosition(row, col);
+                ChessPiece pieceAtSpace = board.getPiece(potentialSpace);
+
+                if(pieceAtSpace == null) {
+                    moves.add(new ChessMove(position, potentialSpace, null));
+                }
+                else {
+                    if(pieceAtSpace.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                        moves.add(new ChessMove(position, potentialSpace, null));
+                    }
+                    break;
+                }
+            }
+        }
+        return moves;
     }
 }
