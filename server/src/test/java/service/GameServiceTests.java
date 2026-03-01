202a3801cae59ea3d987bb168a8b14fd2f261181
austin006 <austinmcglashan1@gmail.com>
2026-03-01 10:20:58 -0700

    Created and tested all GameServiceTests

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -4,8 +4,10 @@ import dataaccess.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.request.CreateGameRequest;
+import service.request.JoinGameRequest;
 import service.request.RegisterRequest;
 import service.result.CreateGameResult;
+import service.result.ListGamesResult;
 import service.result.RegisterResult;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -65,12 +67,107 @@ public class GameServiceTests {
 
     // LISTGAMES TESTS
     // Positive test
+    @Test
+    public void listGamesSuccess() throws DataAccessException {
+        // register person in database
+        RegisterRequest registerRequest = new RegisterRequest("chessplayer", "epic_password", "chess@gmail.com");
+        RegisterResult registerResult = userService.register(registerRequest);
+
+        // save authToken
+        String authToken = registerResult.authToken();
+
+        // create 2 games
+        CreateGameRequest request1 = new CreateGameRequest("cool_game");
+        gameService.createGame(authToken, request1);
+        CreateGameRequest request2 = new CreateGameRequest("epic_game");
+        gameService.createGame(authToken, request2);
+
+        // list games
+        ListGamesResult result = gameService.listGames(authToken);
+
+        // Check it is not null
+        assertNotNull(result);
+
+        // check there are two games
+        assertEquals(2, result.games().size());
+    }
 
     // Negative test - bad token
+    @Test
+    public void listGamesFail() throws DataAccessException {
+        // register person in database
+        RegisterRequest registerRequest = new RegisterRequest("chessplayer", "epic_password", "chess@gmail.com");
+        RegisterResult registerResult = userService.register(registerRequest);
+
+        // save authToken and bad one too
+        String authToken = registerResult.authToken();
+        String authTokenBad = "bad_authToken";
+
+        // create 2 games
+        CreateGameRequest request1 = new CreateGameRequest("cool_game");
+        gameService.createGame(authToken, request1);
+        CreateGameRequest request2 = new CreateGameRequest("epic_game");
+        gameService.createGame(authToken, request2);
+
+        // try to list games
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            gameService.listGames(authTokenBad);
+        });
+
+        // Check error message matches
+        assertEquals("Error: unauthorized", exception.getMessage());
+    }
 
     // JOINGAME TESTS
     // Positive test
+    @Test
+    public void joinGamesSuccess() throws DataAccessException {
+        // register person in database
+        RegisterRequest registerRequest = new RegisterRequest("chessplayer", "epic_password", "chess@gmail.com");
+        RegisterResult registerResult = userService.register(registerRequest);
+
+        // save authToken
+        String authToken = registerResult.authToken();
+
+        // create game
+        CreateGameRequest createGameRequest = new CreateGameRequest("cool_game");
+        CreateGameResult createGameResult = gameService.createGame(authToken, createGameRequest);
+
+        // Join game
+        JoinGameRequest request = new JoinGameRequest("WHITE", createGameResult.gameID());
+        gameService.joinGame(authToken, request);
+
+        // Check it's updated in database
+        assertEquals("chessplayer", gameDAO.getGame(createGameResult.gameID()).whiteUsername());
+    }
 
     // Negative test - Spot already taken
+    @Test
+    public void joinGamesFail() throws DataAccessException {
+        // register 2 people in database
+        RegisterRequest registerRequest = new RegisterRequest("chessplayer", "epic_password", "chess@gmail.com");
+        RegisterResult registerResult = userService.register(registerRequest);
+        RegisterRequest registerRequest2 = new RegisterRequest("chessplayer2", "epic_password2", "chess2@gmail.com");
+        RegisterResult registerResult2 = userService.register(registerRequest2);
 
+        // save authToken
+        String authToken = registerResult.authToken();
+        String authToken2 = registerResult2.authToken();
+
+        // create game
+        CreateGameRequest createGameRequest = new CreateGameRequest("cool_game");
+        CreateGameResult createGameResult = gameService.createGame(authToken, createGameRequest);
+
+        // Join game
+        JoinGameRequest request = new JoinGameRequest("WHITE", createGameResult.gameID());
+        gameService.joinGame(authToken, request);
+
+        // try to join game with second person
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            gameService.joinGame(authToken2, request);
+        });
+
+        // Check error message matches
+        assertEquals("Error: already taken", exception.getMessage());
+    }
 }
