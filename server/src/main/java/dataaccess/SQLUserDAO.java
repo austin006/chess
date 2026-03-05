9b5ed2743b6c9e7070b8cdb0e63d93d4fcacc525
austin006 <austinmcglashan1@gmail.com>
2026-03-04 21:03:53 -0700

    Implemented methods in SQLUserDAO.java and SQLAuthDAO.java

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -2,22 +2,54 @@ package dataaccess;
 
 import model.UserData;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLUserDAO implements UserDAO{
     @Override
     public void clear() throws DataAccessException {
+        var statement = "TRUNCATE users";
 
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to clear users: %s", e.getMessage()));
+        }
     }
 
     @Override
     public void createUser(UserData user) throws DataAccessException {
+        var statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
 
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+            ps.setString(1, user.username());
+            ps.setString(2, user.password());
+            ps.setString(3, user.email());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to create user: %s", e.getMessage()));
+        }
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
+        var statement = "SELECT username, password, email FROM users WHERE username = ?";
+
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement)) {
+
+            ps.setString(1, username);
+
+            try (var rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+
         return null;
     }
 
