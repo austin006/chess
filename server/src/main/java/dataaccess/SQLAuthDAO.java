d13ab35d0a830d13d88f8bfa0da006cf4997df49
austin006 <austinmcglashan1@gmail.com>
2026-03-04 21:32:52 -0700

    Implemented methods in SQLUserDAO.java, SQLAuthDAO.java, and SQLGameDAO.java

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -2,24 +2,68 @@ package dataaccess;
 
 import model.AuthData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class SQLAuthDAO implements AuthDAO{
     @Override
     public void clear() throws DataAccessException {
+        var statement = "TRUNCATE authTokens";
 
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to clear authTokens: %s", e.getMessage()));
+        }
     }
 
     @Override
     public void createAuth(AuthData authData) throws DataAccessException {
+        var statement = "INSERT INTO authTokens (authToken, username) VALUES (?, ?)";
 
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+            ps.setString(1, authData.authToken());
+            ps.setString(2, authData.username());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to create authData: %s", e.getMessage()));
+        }
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
+        var statement = "SELECT authToken, username FROM authTokens WHERE authToken = ?";
+
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement)) {
+
+            ps.setString(1, authToken);
+
+            try (var rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new AuthData(rs.getString("authToken"), rs.getString("username"));
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+
         return null;
     }
 
     @Override
     public void deleteAuth(String authToken) throws DataAccessException {
+        var statement = "DELETE FROM authTokens WHERE authToken = ?";
 
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement)) {
+            ps.setString(1, authToken);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to delete authToken: %s", e.getMessage()));
+        }
     }
 }
--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,33 +1,119 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.GameData;
+import chess.ChessGame;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
-public class SQLGameDAO implements GameDAO{
+public class SQLGameDAO implements GameDAO {
     @Override
     public void clear() throws DataAccessException {
+        var statement = "TRUNCATE TABLE games";
 
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to clear games: %s", e.getMessage()));
+        }
     }
 
     @Override
     public void createGame(GameData game) throws DataAccessException {
+        var statement = "INSERT INTO games (gameID, whiteUsername, blackUsername, gameName, game) VALUES (?, ?, ?, ?, ?)";
+        var serializer = new Gson();
 
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+            ps.setInt(1, game.gameID());
+            ps.setString(2, game.whiteUsername());
+            ps.setString(3, game.blackUsername());
+            ps.setString(4, game.gameName());
+            ps.setString(5, serializer.toJson(game.game()));
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to create game: %s", e.getMessage()));
+        }
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
+        var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM games WHERE gameID = ?";
+        var serializer = new Gson();
+
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement)) {
+
+            ps.setInt(1, gameID);
+
+            try (var rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    ChessGame chessGame = serializer.fromJson(rs.getString("game"), ChessGame.class);
+
+                    return new GameData(
+                            rs.getInt("gameID"),
+                            rs.getString("whiteUsername"),
+                            rs.getString("blackUsername"),
+                            rs.getString("gameName"),
+                            chessGame
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+
         return null;
     }
 
     @Override
     public Collection<GameData> listGames() throws DataAccessException {
-        return List.of();
+        var result = new ArrayList<GameData>();
+        var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM games";
+        var serializer = new Gson();
+
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement);
+             var rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                ChessGame chessGame = serializer.fromJson(rs.getString("game"), ChessGame.class);
+
+                result.add(new GameData(
+                        rs.getInt("gameID"),
+                        rs.getString("whiteUsername"),
+                        rs.getString("blackUsername"),
+                        rs.getString("gameName"),
+                        chessGame
+                ));
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to list games: %s", e.getMessage()));
+        }
+
+        return result;
     }
 
     @Override
     public void updateGame(GameData game) throws DataAccessException {
+        var statement = "UPDATE games SET whiteUsername = ?, blackUsername = ?, gameName = ?, game = ? WHERE gameID = ?";
+        var serializer = new Gson();
 
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+            ps.setString(1, game.whiteUsername());
+            ps.setString(2, game.blackUsername());
+            ps.setString(3, game.gameName());
+            ps.setString(4, serializer.toJson(game.game()));
+            ps.setInt(5, game.gameID());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to update game: %s", e.getMessage()));
+        }
     }
 }
