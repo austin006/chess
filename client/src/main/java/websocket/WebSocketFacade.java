aa36a6850ac8682dc9d79a4d8a490eae62b4cfd6
austin006 <austinmcglashan1@gmail.com>
2026-03-28 20:49:04 -0600

    Created WebSocketFacade

---
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -0,0 +1,79 @@
+package websocket;
+
+import com.google.gson.Gson;
+import client.ResponseException;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ServerMessage;
+
+import jakarta.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebSocketFacade extends Endpoint {
+
+    private Session session;
+    private ServerMessageObserver observer;
+
+    public WebSocketFacade(String url, ServerMessageObserver observer) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.observer = observer;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+
+                    switch (serverMessage.getServerMessageType()) {
+                        case NOTIFICATION -> {
+                            NotificationMessage notification = new Gson().fromJson(message, NotificationMessage.class);
+                            observer.notify(notification);
+                        }
+                        case LOAD_GAME -> {
+                            LoadGameMessage loadGame = new Gson().fromJson(message, LoadGameMessage.class);
+                            observer.notify(loadGame);
+                        }
+                        case ERROR -> {
+                             ErrorMessage error = new Gson().fromJson(message, ErrorMessage.class);
+                             observer.notify(error);
+                        }
+                    }
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    // CHESS COMMANDS
+
+    public void connect(String authToken, int gameID) throws ResponseException {
+        try {
+            var action = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    public void leave(String authToken, int gameID) throws ResponseException {
+        try {
+            var action = new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+}
